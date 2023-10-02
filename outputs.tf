a0a2ba9f41e22f82f4d1ad347cec5bfe6002b119
Jasmine Rios <livejasminerios@gmail.com>
2023-10-02 18:58:10 +0000

    #27 enabled static website hosting, uploaded index and error file via terraform

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
   description = "Bucket name for our static website hosting"
   value = module.terrahouse_aws.bucket_name
 }
+
+output "S3_website_endpoint" {
+  description = "s3 website static hosting website"
+  value = module.terrahouse_aws.website_endpoint
+}
