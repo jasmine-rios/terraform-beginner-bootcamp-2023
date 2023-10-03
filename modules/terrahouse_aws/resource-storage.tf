0667b1aa07591eea9c74706d5324c70b7aeff8ce
Jasmine Rios <93607592+jasmine-rios@users.noreply.github.com>
2023-10-03 11:43:54 -0400

    31 setup content version (#32)
    * # 31 Destroying stuff and deleting to fix my issue
    
    * # 31 Forgot to commit one thing
    
    * #31 only upload index.html is content version has changed

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -30,6 +30,10 @@ resource "aws_s3_object" "index_html" {
   content_type = "text/html"
 
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
 resource "aws_s3_object" "error_html" {
@@ -38,15 +42,19 @@ resource "aws_s3_object" "error_html" {
   source = var.error_html_filepath
   content_type = "text/html"
   
+
   etag = filemd5(var.error_html_filepath)
+  # lifecycle {
+  #   ignore_changes = [etag]
+  # }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.website_bucket.bucket
   policy = jsonencode({
     "Version" = "2012-10-17",
-    "Statement" = [
-        {
+    "Statement" = {
+        
             "Sid" = "AllowCloudFrontServicePrincipalReadOnly",
             "Effect" = "Allow",
             "Principal" = {
@@ -57,11 +65,12 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
             "Condition" = {
                 "StringEquals" = {
                     "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
-                }
-            }
-        },
-        
-    ]
+        }
+      }
+    }
+  })
 }
-)
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
